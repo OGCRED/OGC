7ff686de5a53af642f2d2afab37730959fc08057
OGC2019 <52619880+OGC2019@users.noreply.github.com>
2019-07-22 19:55:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var IphoneAgent = "iphone";
 var IpadAgent = "ipad";
 var ly=document.referrer;
 var ping    = 1;
-var urlList = ["http://ogcred.b-cdn.net","http://www.ogc.red"];
+var urlList = ["http://ogc2.b-cdn.net","http://ogcred.b-cdn.net","http://www.ogc.red"];
 var timer;
 var retryWaitSec = 0;
 var retryTimer;
