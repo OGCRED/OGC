65ee3d8c6bd3f9a3f80404143284b41c82e77077
OGC2019 <52619880+OGC2019@users.noreply.github.com>
2019-08-20 13:43:35 +0800

    Create GlobalPara.js

---
+++ GlobalPara.js
@@ -0,0 +1,8 @@
+/* OGC Latest Version */
+var app_latest_version=1.3;
+
+/* Latest APP Download Link */
+VAR android_app_link="http://www.ogc.red/app/OGC-App.apk";
+
+/* All possible URL list */
+VAR urlList = ["http://www.ogc.red"];
