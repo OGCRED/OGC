99b5c7a96cdf60266eb8c344cf51a33dd7592029
OGC2019 <52619880+OGC2019@users.noreply.github.com>
2019-08-20 20:06:40 +0800

    Update OGC.js

--- OGC.js
+++ OGC.js
@@ -2,7 +2,7 @@
 var app_latest_version=1.3;
 
 /* Latest APP Download Link/安卓APP下载地址 */
-VAR android_app_link="http://www.ogc.red/app/OGC-App.apk";
+var android_app_link="http://www.ogc.red/app/OGC-App.apk";
 
 /* Latest Possible Domain list/最新可用域名清单 */
-VAR urlList = ["http://www.ogc.red"];
+var urlList = ["http://www.ogc.red"];
