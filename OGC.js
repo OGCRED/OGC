f23f0f79cadc3a599206d97aa41c2213ddc68d3d
OGC2019 <52619880+OGC2019@users.noreply.github.com>
2019-08-20 13:49:27 +0800

    OGC撸管网最新信息

--- OGC.js
+++ OGC.js
@@ -1,8 +1,8 @@
-/* OGC Latest Version */
+/* OGC Latest Version/最新APP版本 */
 var app_latest_version=1.3;
 
-/* Latest APP Download Link */
+/* Latest APP Download Link/安卓APP下载地址 */
 VAR android_app_link="http://www.ogc.red/app/OGC-App.apk";
 
-/* All possible URL list */
+/* Latest Possible Domain list/最新可用域名清单 */
 VAR urlList = ["http://www.ogc.red"];
