8f382b95034211f6507dc0d1d0fc6a19f4f29906
OGCRED <52619880+OGCRED@users.noreply.github.com>
2019-08-21 12:19:23 +0800

    Update OGC.js

--- OGC.js
+++ OGC.js
@@ -1,5 +1,5 @@
 /* OGC Latest Version/最新APP版本 */
-var app_latest_version=1.3;
+var app_latest_version=1.5;
 
 /* Latest APP Download Link/安卓APP下载地址 */
 var android_app_link="http://www.ogc.red/app/OGC-App.apk";
